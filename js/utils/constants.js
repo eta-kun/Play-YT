e5f6f5f3dd507ad0d99ae410a0d65315e9beaa33
えたー <sea.eita@gmail.com>
2026-06-06 23:06:22 +0900

    Update constants.js

--- js/utils/constants.js
+++ js/utils/constants.js
@@ -1,7 +1,7 @@
 export const CONFIG = {
   // Replace these values before publishing. Keep OAuth redirect origins in Google Cloud
   // limited to your GitHub Pages URL and local test URL.
-  GOOGLE_CLIENT_ID: "588623947338-alontiigeie7ncsarjplb8lqk91avap1.apps.googleusercontent.com",
+  GOOGLE_CLIENT_ID: "YOUR_GOOGLE_OAUTH_CLIENT_ID.apps.googleusercontent.com",
   YOUTUBE_API_KEY: "YOUR_YOUTUBE_DATA_API_KEY",
   SCOPES: [
     "https://www.googleapis.com/auth/youtube.readonly"
@@ -23,7 +23,16 @@ export const STORAGE_KEYS = {
   auth: "pulsetube.auth",
   settings: "pulsetube.settings",
   queue: "pulsetube.queue",
-  searchHistory: "pulsetube.searchHistory"
+  searchHistory: "pulsetube.searchHistory",
+  quotaUsage: "pulsetube.quotaUsage"
+};
+
+export const QUOTA = {
+  DAILY_LIMIT: 10000,
+  COSTS: {
+    search: 100,
+    default: 1
+  }
 };
 
 export const SPECIAL_PLAYLISTS = {
